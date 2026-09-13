ba9fd2f8a57c080f690dc9b09d07a4f3ccd77a19
muttanagouda <muthunsuman2025@gmail.com>
2026-09-13 14:32:36 +0530

    add class java

--- src/main/java/com/jtspringproject/JtSpringProject/controller/AdminController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/AdminController.java
@@ -3,9 +3,6 @@ package com.jtspringproject.JtSpringProject.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,19 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.jtspringproject.JtSpringProject.models.Category;
-import com.jtspringproject.JtSpringProject.models.Product;
-import com.jtspringproject.JtSpringProject.models.User;
-import com.jtspringproject.JtSpringProject.services.categoryService;
-import com.jtspringproject.JtSpringProject.services.userService;
+
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
-	@GetMapping("/")
+    @GetMapping("/")
     public String hello() {
-        return "Hello World from springboot!";
+        return "Hello World from Spring Boot!";
     }
-
 }
